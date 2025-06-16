e20777b733b1b44e6ab301d2902e9250a0e94221
fisheeesh <swanphyo444@gmail.com>
2025-06-16 17:54:20 +0700

    chore: layout header

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+        <header className="w-full border-b">
+            <div className="container flex items-center h-16">
+                {/* Navigations */}
+            </div>
+        </header>
+    )
+}
--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/layout/Header";
 import { Outlet } from "react-router";
 
 export default function RootLayout() {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
+            <Header />
             <Outlet />
         </div>
     )
