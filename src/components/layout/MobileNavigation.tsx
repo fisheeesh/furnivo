4f14b0715172617c2a77aad56f7074da8484d60b
fisheeesh <swanphyo444@gmail.com>
2025-06-17 19:35:45 +0700

    chore: finished up mobile nav

--- src/components/layout/MobileNavigation.tsx
+++ src/components/layout/MobileNavigation.tsx
@@ -3,10 +3,20 @@ import type { MainNavItem } from "@/types"
 import { Button } from "@/components/ui/button"
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetTrigger
 } from "@/components/ui/sheet"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 import { Icons } from "../icons"
+import { Link } from "react-router"
+import { siteConfig } from "@/config/site"
+import { ScrollArea } from "../ui/scroll-area"
 
 interface MainNavigationProps {
     items?: MainNavItem[]
@@ -22,7 +32,47 @@ export default function MobileNavigation({ items }: MainNavigationProps) {
                         <span className="sr-only">Toggle Menu</span>
                     </Button>
                 </SheetTrigger>
-                <SheetContent side="left" className="pl-1 pr-0 pt-9">
+                <SheetContent side="left" className="py-9 px-5">
+                    <SheetClose asChild>
+                        <Link to='/' className="flex items-center gap-2">
+                            <Icons.logo className="size-7" />
+                            <span className="font-bold">{siteConfig.name}</span>
+                            <span className="sr-only">Home</span>
+                        </Link>
+                    </SheetClose>
+
+                    <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-8">
+                        <Accordion
+                            type="multiple"
+                            className="w-full border-b"
+                        >
+                            <AccordionItem value="item-1">
+                                <AccordionTrigger className="">{items?.[0]?.title}</AccordionTrigger>
+                                <AccordionContent className="flex flex-col space-y-3 pl-2">
+                                    {
+                                        items?.[0].card?.map(item => (
+                                            <SheetClose asChild key={item.href}>
+                                                <Link to={String(item.href)} className="text-muted-foreground">
+                                                    {item.title}
+                                                </Link>
+                                            </SheetClose>
+                                        ))
+                                    }
+                                </AccordionContent>
+                            </AccordionItem>
+                        </Accordion>
+                        <div className="flex flex-col space-y-2 mt-4">
+                            {
+                                items?.[0]?.menu?.map(item => (
+                                    <SheetClose asChild key={item.href}>
+                                        <Link to={String(item.href)} className="">
+                                            {item.title}
+                                        </Link>
+                                    </SheetClose>
+                                ))
+                            }
+                        </div>
+                    </ScrollArea>
                 </SheetContent>
             </Sheet>
         </div>
