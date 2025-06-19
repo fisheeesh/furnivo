48bef7dd58c170d69743b46e7a088adc58ae0624
fisheeesh <swanphyo444@gmail.com>
2025-06-19 17:53:14 +0700

    chore: format hero section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,31 @@
-import { Button } from "../components/ui/button";
+import { Button } from '@/components/ui/button'
+import Couch from '@/data/images/couch.png'
+import { Link } from 'react-router'
+
 
 export default function Home() {
   return (
-    <div>
-      <Button variant='default'>Click Me</Button>
+    <div className='container mx-auto'>
+      <div className='flex flex-col lg:flex-row lg:justify-between items-center'>
+        {/* Text Sectioin */}
+        <div className='text-center lg:text-start'>
+          <h1>Modern Interior Design Studio</h1>
+          <p>
+            Furniture is an essential component of any living space, providing
+            functionality, comfort, and aesthietic appeal.
+          </p>
+          <div>
+            <Button asChild>
+              <Link to="#">Shop Now</Link>
+            </Button>
+            <Button>
+              <Link to="#">Explore</Link>
+            </Button>
+          </div>
+        </div>
+        {/* Image Section */}
+        <img src={Couch} alt="couch" />
+      </div>
     </div>
   )
 }
