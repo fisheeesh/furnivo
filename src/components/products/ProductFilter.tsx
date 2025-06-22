f4745eaa56b42bf53f69fb739c7ee54d617f0064
fisheeesh <swanphyo444@gmail.com>
2025-06-22 21:32:59 +0700

    chore: format products page layout

---
+++ src/components/products/ProductFilter.tsx
@@ -0,0 +1,8 @@
+
+export default function ProductFilter() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/pages/products/Product.tsx
+++ src/pages/products/Product.tsx
@@ -1,8 +1,28 @@
+import Pagination from "@/components/products/Pagination";
+import ProductCard from "@/components/products/ProductCard";
+import ProductFilter from "@/components/products/ProductFilter";
+import { filterList, products } from "@/data/products";
 
 export default function Product() {
     return (
-        <div>
-
+        <div className="">
+            <section className="">
+                <section className="">
+                    <ProductFilter title="Furnitures Made By" filterList={filterList.categories} />
+                    <ProductFilter title="Furnitures Types" filterList={filterList.types} />
+                </section>
+                <section className="">
+                    <h1 className="">All Products</h1>
+                    <div className="">
+                        {
+                            products.map(product => (
+                                <ProductCard key={product.id} product={product} />
+                            ))
+                        }
+                    </div>
+                    <Pagination />
+                </section>
+            </section>
         </div>
     )
 }
