ec2ac1ed98142506b735f18b641df08c0f6129a2
fisheeesh <swanphyo444@gmail.com>
2025-06-24 19:09:56 +0700

    chore: changed app logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 export default function Logo({ className }: { className?: string }) {
     return (
         <Link to='/' className={cn('flex items-center space-x-2 text-lg tracking-tight text-foreground/80 hover:text-foreground transition-colors duration-200', className)}>
-            <Icons.logo className="size-7" aria-hidden="true" />
+            <Icons.logo className="size-6" aria-hidden="true" />
             <span className="font-bold">Furniture Shop</span>
             <span className="sr-only">Home</span>
         </Link>
