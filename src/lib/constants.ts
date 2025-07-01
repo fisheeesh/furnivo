f5c76c579b55d8ee196e8b0e1a8a6b6775ed9401
fisheeesh <swanphyo444@gmail.com>
2025-07-02 00:17:02 +0700

    folder changed

--- src/lib/constants.ts
+++
@@ -1,8 +0,0 @@
-export const LOGIN = "Login"
-export const REGISTER = "Register"
-
-export const LOGIN_TITLE = "Login to your account"
-export const LOGIN_SUBTITLE = "Enter your phone number below to login to your account"
-
-export const REGISTER_TITLE = "Create your account"
-export const REGISTER_SUBTITLE = "Enter your phone number below to create your account"
