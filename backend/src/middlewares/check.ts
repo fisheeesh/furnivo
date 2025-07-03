3243a8390d4873fb1cf3679d0351256acb707c71
fisheeesh <swanphyo444@gmail.com>
2025-07-03 12:48:59 +0700

    chore: create custom middleware

---
+++ backend/src/middlewares/check.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express"
+
+interface CustomRequest extends Request {
+    userId?: number
+}
+
+export const check = (req: CustomRequest, res: Response, next: NextFunction) => {
+    req.userId = 12345
+    next()
+}
