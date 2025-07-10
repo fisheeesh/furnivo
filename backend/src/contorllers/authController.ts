2e60ea77148f618d350bbca247e55df719fc4209
fisheeesh <swanphyo444@gmail.com>
2025-07-10 17:55:40 +0700

    feat: implemeted otp limit per day feature

--- backend/src/contorllers/authController.ts
+++ backend/src/contorllers/authController.ts
@@ -2,8 +2,8 @@ import { NextFunction, Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
 import bcrypt from 'bcrypt'
 
-import { getUserByPhone, createOTP } from '../services/authServices'
-import { checkUserExit } from '../utils/auth'
+import { getUserByPhone, createOTP, getOTPByPhone, updateOTP } from '../services/authServices'
+import { checkOTPErrorIfSameDate, checkUserExit } from '../utils/auth'
 import { generateOTP, generateToken } from '../utils/generate'
 
 export const register = [
@@ -37,15 +37,58 @@ export const register = [
         const salt = await bcrypt.genSalt(10)
         const hashOtp = await bcrypt.hash(otp.toString(), salt)
         const token = generateToken()
-        const otpData = {
-            phone,
-            otp: hashOtp,
-            rememberToken: token,
-            count: 1
+
+        const otpRow = await getOTPByPhone(phone)
+        let result;
+        if (!otpRow) {
+            const otpData = {
+                phone,
+                otp: hashOtp,
+                rememberToken: token,
+                count: 1
+            }
+
+            //* Save OTP to DB
+            result = await createOTP(otpData)
+        } else {
+            const lastOtpRequest = new Date(otpRow.updatedAt).toLocaleDateString()
+            const today = new Date().toLocaleDateString()
+            const isSameDate = lastOtpRequest === today
+
+            checkOTPErrorIfSameDate(isSameDate, otpRow.error)
+
+            if (!isSameDate) {
+                const otpData = {
+                    phone,
+                    otp: hashOtp,
+                    rememberToken: token,
+                    count: 1,
+                    error: 0
+                }
+
+                result = await updateOTP(otpRow.id, otpData)
+            } else {
+                if (otpRow.count === 3) {
+                    const error: any = new Error(
+                        "OTP is allowed to request 3 times per day."
+                    )
+                    error.status = 405
+                    error.code = 'Error_OverLimit'
+                    return next(error)
+                } else {
+                    const otpData = {
+                        phone,
+                        otp: hashOtp,
+                        rememberToken: token,
+                        count: { increment: 1 },
+                    }
+
+                    result = await updateOTP(otpRow.id, otpData)
+                }
+            }
         }
 
-        //* Save OTP to DB
-        const result = await createOTP(otpData)
+
 
         res.status(200).json({
             message: `We are sending OTP to 09${result.phone}`,
--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -6,3 +6,14 @@ export const checkUserExit = (user: any) => {
         throw error
     }
 }
+
+export const checkOTPErrorIfSameDate = (isSameDate: boolean, errorCount: number) => {
+    if (isSameDate && errorCount >= 5) {
+        const error: any = new Error(
+            "OTP is wrong for 5 times. Please try again tomorrow."
+        )
+        error.status = 401
+        error.code = 'Error_OverLimit'
+        throw error
+    }
+}
