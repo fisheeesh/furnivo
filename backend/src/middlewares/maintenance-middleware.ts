c9b3dd1f73969bbffe81b42be7543b4dd72d04e2
fisheeesh <swanphyo444@gmail.com>
2025-07-21 15:41:07 +0700

    feat: whitelist ip in maintenance mode

--- backend/src/middlewares/maintenance-middleware.ts
+++ backend/src/middlewares/maintenance-middleware.ts
@@ -3,15 +3,26 @@ import { getSettignStatus } from "../services/system-service";
 import { errorCode } from "../config/error-code";
 import { createHttpError } from "../utils/auth";
 
-export const maintenance = async (req: Request, res: Response, next: NextFunction) => {
-    const setting = await getSettignStatus("maintenance")
-    if (setting?.value === 'true') {
-        return next(createHttpError({
-            message: "The server is under maintenance. Please try again later.",
-            code: errorCode.maintenance,
-            status: 503
-        }))
-    }
+const whiteLists = ["127.0.0.1"]
 
-    next()
+export const maintenance = async (req: Request, res: Response, next: NextFunction) => {
+    const ip: any = req.headers["x-forwarded-for"] || req.socket.remoteAddress
+    
+    if (whiteLists.includes(ip)) {
+        console.log(`Allowed IP: ${ip}`)
+        next()
+    } else {
+        console.log(`Not Allowed IP: ${ip}`)
+
+        const setting = await getSettignStatus("maintenance")
+        if (setting?.value === 'true') {
+            return next(createHttpError({
+                message: "The server is under maintenance. Please try again later.",
+                code: errorCode.maintenance,
+                status: 503
+            }))
+        }
+
+        next()
+    }
 }
