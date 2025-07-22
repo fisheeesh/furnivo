bc07df0da434ca8fe0a75e48abbde37761beb0d3
fisheeesh <swanphyo444@gmail.com>
2025-07-22 16:39:06 +0700

    feat: filter file from req and limit its size

--- backend/src/middlewares/upload-file.ts
+++ backend/src/middlewares/upload-file.ts
@@ -15,13 +15,13 @@ const fileStorage = multer.diskStorage({
     }
 })
 
-// const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-//     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/webp') {
-//         cb(null, true)
-//     } else {
-//         cb(null, false)
-//     }
-// }
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/webp') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
 
 const upload = multer({
     storage: fileStorage,
