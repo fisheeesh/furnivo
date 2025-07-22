9d6ff0f530f1056c08ecffa7420f660e5c22263f
fisheeesh <swanphyo444@gmail.com>
2025-07-22 17:46:04 +0700

    feat: implemented multiple files upload

--- backend/src/routes/v1/user/user-routes.ts
+++ backend/src/routes/v1/user/user-routes.ts
@@ -1,12 +1,16 @@
 import express from "express"
-import { changeLanguage, testPermission, uploadProfile } from "../../../controllers/user/user-controller"
+import { changeLanguage, getMyPhoto, testPermission, uploadProfile, uploadProfileMultiple, uploadProfileOptimize } from "../../../controllers/user/user-controller"
 import { auth } from "../../../middlewares/auth-middleware"
-import upload from "../../../middlewares/upload-file"
+import upload, { uploadMemory } from "../../../middlewares/upload-file"
 
 const router = express.Router()
 
 router.post('/change-language', changeLanguage)
 router.get('/test-permission', auth, testPermission)
 router.patch("/profile/upload", auth, upload.single("avatar"), uploadProfile)
+router.patch("/profile/upload/optimize", auth, uploadMemory.single("avatar"), uploadProfileOptimize)
+router.patch("/profile/upload/multiple", auth, upload.array('avatar'), uploadProfileMultiple)
+
+router.get('/profile/my-photo', getMyPhoto)
 
 export default router
