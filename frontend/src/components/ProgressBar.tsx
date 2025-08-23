1794a5e918e617c6007184400287a40935514fd0
fisheeesh <swanphyo444@gmail.com>
2025-08-23 23:49:34 +0700

    feat: use nprogress bar for app level loading state for better ux

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -1,17 +1,20 @@
+import { useEffect } from "react"
+import NProgress from "nprogress"
+import "nprogress/nprogress.css"
 import { useIsFetching } from "@tanstack/react-query"
 import { useNavigation } from "react-router"
 
 export default function ProgressBar() {
+    const fetching = useIsFetching()
     const navigation = useNavigation()
-    const fetching = useIsFetching() > 0
 
-    if (fetching || navigation.state !== 'idle') {
-        return (
-            <div className="fixed left-0 top-0 z-50 h-0.5 w-full overflow-hidden bg-gray-200">
-                <div className="absolute h-full w-2/3 animate-progress bg-own" />
-            </div>
-        )
-    }
+    useEffect(() => {
+        if (fetching > 0 || navigation.state !== "idle") {
+            NProgress.start()
+        } else {
+            NProgress.done()
+        }
+    }, [fetching, navigation.state])
 
     return null
 }
