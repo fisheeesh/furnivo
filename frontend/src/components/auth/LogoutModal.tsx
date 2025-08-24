ad554c373c182103d757ed7a57bdc8374b01c681
fisheeesh <swanphyo444@gmail.com>
2025-08-25 00:07:54 +0700

    fix: after logout user can go to home page fixed by removing all queries as soon as logout and navigate back to home screen for avoid navigation state mismatch

--- frontend/src/components/auth/LogoutModal.tsx
+++ frontend/src/components/auth/LogoutModal.tsx
@@ -7,12 +7,41 @@ import {
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog"
-import { Form, useNavigation } from "react-router"
 import Spinner from "../Spinner"
+import { authApi } from "@/api"
+import { queryClient } from "@/api/query"
+import { useMutation } from "@tanstack/react-query"
+import { useNavigate } from "react-router"
+import { toast } from "sonner"
 
 export default function LogoutModal() {
-    const navigation = useNavigation()
-    const isWorking = navigation.state === 'submitting'
+    // const navigation = useNavigation()
+    // const isWorking = navigation.state === 'submitting'
+
+    const navigate = useNavigate()
+
+    const { mutate, isPending: isWorking } = useMutation({
+        mutationFn: async () => {
+            const res = await authApi.post("/logout")
+
+            if (res.status !== 200) {
+                throw new Error("Something went wrong. Please try again.")
+            }
+
+            return res.data
+        },
+        onSuccess: () => {
+            //? After success logout, we need to remove the current user data from react query cache even though that is removed from the local storage by server.
+            //? In here, we remove all of the queries from the cache.
+            queryClient.removeQueries()
+            navigate('/', { replace: true })
+        },
+        onError: (error) => {
+            toast.error("Error", {
+                description: error instanceof Error ? error.message : "Something went wrong. Please try again."
+            })
+        }
+    })
 
     return (
         <DialogContent className="sm:max-w-[425px]">
@@ -26,13 +55,18 @@ export default function LogoutModal() {
                 <DialogClose asChild>
                     <Button disabled={isWorking} variant="outline" className="cursor-pointer">Cancel</Button>
                 </DialogClose>
-                <Form method="POST" action="/logout">
+                {/* <Form method="POST" action="/logout">
                     <Button disabled={isWorking} type="submit" className="cursor-pointer">
                         <Spinner isLoading={isWorking} label={'Loggint out...'}>
                             Confirm
                         </Spinner>
                     </Button>
-                </Form>
+                </Form> */}
+                <Button onClick={() => mutate()} disabled={isWorking} type="submit" className="cursor-pointer">
+                    <Spinner isLoading={isWorking} label={'Loggint out...'}>
+                        Confirm
+                    </Spinner>
+                </Button>
             </DialogFooter>
         </DialogContent>
     )
