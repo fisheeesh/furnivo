b012742a28d3ec1f44c2bb2634b56dc482cd0d3c
fisheeesh <swanphyo444@gmail.com>
2025-08-25 17:14:19 +0700

    fix: nprogress bar issue fixed

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -1,22 +1,32 @@
-import { useEffect } from "react"
+import { useIsFetching } from "@tanstack/react-query"
 import NProgress from "nprogress"
 import "nprogress/nprogress.css"
-import { useIsFetching } from "@tanstack/react-query"
+import { useEffect } from "react"
 import { useNavigation } from "react-router"
 
 export default function ProgressBar() {
     const fetching = useIsFetching()
     const navigation = useNavigation()
 
+    //* Routes where we don't want to show progress bar
+    const skipProgressRoutes = ['/login', '/register', '/forgot-password']
+
+    //* Check if we're navigating to a route that should skip progress
+    const shouldSkipProgress = navigation.location &&
+        skipProgressRoutes.includes(navigation.location.pathname)
+
     useEffect(() => {
+        if (shouldSkipProgress) {
+            NProgress.done()
+            return
+        }
+
         if (fetching > 0 || navigation.state !== "idle") {
-            console.log(navigation.state)
             NProgress.start()
         } else {
-            console.log(navigation.state)
             NProgress.done()
         }
-    }, [fetching, navigation.state])
+    }, [fetching, navigation.state, shouldSkipProgress])
 
     return null
 }
